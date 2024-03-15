35c245b7e3c908677398e1099b29103ffd5456de
g23fgfgdhs34jsgrhdgfdyeg4hegdf4eyhcfreh <163509455+g23fgfgdhs34jsgrhdgfdyeg4hegdf4eyhcfreh@users.noreply.github.com>
2024-03-15 16:44:22 +0330

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 const fs = require('fs');
 const util = require('util');
-
+const axios = require('axios');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -12,14 +12,16 @@ require('dotenv').config();
 // Read environment variables from .env file
 const mainUrl = process.env.MAIN_URL;
 
+console.log('url=' + mainUrl)
 io.on('connection', (socket) => {
-    socket.on('sendToken', (token) => {
+    socket.on('sendToken', async (token) => {
         console.log(token);
-        if (token === "Hash1") {
-            socket.emit("receiveResponse", token + " received");
-            console.log('Data Received' + token);
-            console.log(' MainURL' + mainUrl); 
-        }
+             qrUrl=mainUrl + "qrCode/index.php?apiKey=" + token
+             const response = await getQrcode(qrUrl);
+             socket.emit("receiveResponse", response);
+            console.log('Data Received' + response);
+           // console.log(' MainURL' + mainUrl); 
+        
      
     });
 
@@ -39,3 +41,25 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
+
+
+// تابع برای ارسال درخواست GET به آدرس mainUrl و ارسال نتیجه به کلاینت
+async function getQrcode(mainUrl) {
+    try {
+        // ارسال درخواست GET به آدرس mainUrl با استفاده از axios
+        const response = await axios.get(mainUrl);
+        
+        // بازگرداندن مقدار دلخواه از پاسخ
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        // ارسال پیغام خطا به کلاینت اگر در ارسال درخواست یا دریافت پاسخ مشکلی رخ داده باشد
+        console.error("Error:", error);
+        return 'error';
+    }
+}
+
+
+
+
